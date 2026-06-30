fba9d479d98b47cfcafbfa831ee53b16d3f00562
Phun Peeticharoenthum <phunphun484@gmail.com>
2026-06-30 20:37:54 +0700

    REMOVE: Unused script

--- Scripts/Editor/SecondBrainCore.cs
+++
@@ -1,3 +0,0 @@
-// SecondBrainCore and ProfileInitializationState have moved to
-// Runtime/Common/SecondBrainCore.cs (namespace SecretZauce.SecondBrain).
-// This file is intentionally empty to preserve its .meta GUID.
